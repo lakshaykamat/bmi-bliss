df94d0ac5220c2d175cb8a20072f3cadefa5f1d5
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2024-02-07 12:47:52 +0530

    Change version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lakshaykamat.bmiBliss"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
